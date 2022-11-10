329d533d9fbd3610d04e5cfc70821eecea266336
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-11-10 18:34:18 +0500

    7-misol

--- file07.py
+++ file07.py
@@ -8,3 +8,12 @@ def main(data:str):
     """
     
 # Read data from file
+    m=0
+    
+    for i in data: 
+        if i.isdigit():
+            m+=int(i)
+            
+            
+    return m
+print(main(open('txt_file/data07.txt').read()))
