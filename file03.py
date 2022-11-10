7ba0268bf14e4c706f8ded121daa19ad94ae4f7b
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-11-10 18:33:46 +0500

    3-misol

--- file03.py
+++ file03.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+ 
 # Read data from file
+    m=[]
+
+    for i in data:
+        if i.isdigit():
+
+            m.append(i)
+    return m
+
+
+
+s=open('txt_file/data03.txt').read()
+print(main(s))
